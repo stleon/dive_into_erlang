0640bf8c1bad02f954759e3e2f680d17369a493e
ST LEON <leonst998@gmail.com>
2017-02-08 23:49:48 +0300

    define macros

--- useless.erl
+++ useless.erl
@@ -1,9 +1,20 @@
 -module(useless).
--export([add/2, hello/0, greet_and_add_two/1]).
--compile([debug_info, export_all]).
+-compile([debug_info, {d, debugmode}]).
+-define(sub(X, Y), X - Y).
+-define(MACRO, 18).
+-ifdef(debugmode).
+-define(DEBUG(S), io:format("dbg: " ++ S)).
+-else.
+-define(DEBUG(S), ok).
+-endif.
+-export([add/2, hello/0, greet_and_add_two/1, use_macro/0]).
 
 add(X, Y) ->
+    ?DEBUG("in add func"),
     X + Y.
+
+use_macro() ->
+    ?MACRO + ?sub(10, 9).
 
 %% hello
 hello() ->
