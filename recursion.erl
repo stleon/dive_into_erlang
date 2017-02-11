1420e1862bd77feeb5833e89acc6372d60008eda
ST LEON <leonst998@gmail.com>
2017-02-11 14:52:01 +0300

    tail recursion and duplicate

--- recursion.erl
+++ recursion.erl
@@ -1,10 +1,26 @@
 -module(recursion).
--export([fac/1, fac/2, len/1, len/2]).
+-export([tail_fac/1, tail_fac/2, tail_len/1, tail_len/2, tail_duplicate/2,
+         tail_duplicate/3]).
 
-fac(N) -> fac(N, 1).
-fac(0, Acc) -> Acc;
-fac(N, Acc) when N > 0 -> fac(N-1, N * Acc).
+fac(0) -> 1;
+fac(N) when N > 0 -> N * fac(N - 1).
 
-len(L) -> len(L, 0).
-len([], Acc) -> Acc;
-len([_|T], Acc) -> len(T, Acc + 1).
+tail_fac(N) -> tail_fac(N, 1).
+tail_fac(0, Acc) -> Acc;
+tail_fac(N, Acc) when N > 0 -> tail_fac(N-1, N * Acc).
+
+len([]) -> 0;
+len([_|T]) -> 1 + len(T).
+
+tail_len(L) -> tail_len(L, 0).
+tail_len([], Acc) -> Acc;
+tail_len([_|T], Acc) -> tail_len(T, Acc + 1).
+
+duplicate(0, _) -> [];
+duplicate(N, Term) when N > 0 ->
+[Term|duplicate(N - 1, Term)].
+
+tail_duplicate(N, Term) -> tail_duplicate(N, Term, []).
+tail_duplicate(0, _, List) -> List;
+tail_duplicate(N, Term, List) when N > 0 -> 
+    tail_duplicate(N - 1, Term, [Term|List]).
