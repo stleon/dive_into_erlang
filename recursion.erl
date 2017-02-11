ec17baa89fc346a678f8661178be8f6a28271b38
ST LEON <leonst998@gmail.com>
2017-02-11 21:46:49 +0300

    tail_sublist

--- recursion.erl
+++ recursion.erl
@@ -1,6 +1,7 @@
 -module(recursion).
 -export([tail_fac/1, tail_fac/2, tail_len/1, tail_len/2, tail_duplicate/2,
-         tail_duplicate/3, tail_reverse/1, tail_reverse/2]).
+         tail_duplicate/3, tail_reverse/1, tail_reverse/2, tail_sublist/2,
+         tail_sublist/3]).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N * fac(N - 1).
@@ -31,3 +32,13 @@ reverse([H|T]) -> reverse(T) ++ [H].
 tail_reverse(L) -> tail_reverse(L, []).
 tail_reverse([], Acc) -> Acc;
 tail_reverse([H|T], Acc) -> tail_reverse(T, [H|Acc]).
+
+sub_list([], _) -> [];
+sub_list(_, 0) -> [];
+sub_list([H|T], N) when N > 0 -> [H|sub_list(T, N - 1)].
+
+tail_sublist(L, N) -> tail_reverse(tail_sublist(L, N, [])). % list:reverse
+tail_sublist([], _, Sublist) -> Sublist;
+tail_sublist(_, 0, Sublist) -> Sublist;
+tail_sublist([H|T], N, Sublist) when N > 0 ->
+    tail_sublist(T, N - 1, [H|Sublist]).
