598d79f8c3281e8140fc1926cde883fcd3d9792c
ST LEON <leonst998@gmail.com>
2017-02-28 22:52:36 +0300

    critic base

--- linkmon.erl
+++ linkmon.erl
@@ -5,3 +5,27 @@
 myproc() ->
     timer:sleep(5000),
     exit(lol).
+
+start_critic() -> 
+    spawn(?MODULE, critic, []).
+
+judge(Pid, Band, Album) ->
+    Pid ! {self(), {Band, Album}},
+    receive
+        {Pid, Criticism} -> Criticism
+    after 2000 ->
+        timeout
+    end.
+
+critic() ->
+    receive
+        {From, {"test_band", "test_album"}} ->
+            From ! {self(), "nice!"};
+        {From, {"test_band2", "test_album2"}} ->
+            From ! {self(), "nice2!"};
+        {From, {"test_band3", "test_album3"}} ->
+            From ! {self(), "nice3!"};
+        {From, _} ->
+            From ! {self(), "fuck off!!"}
+    end,
+    critic().
