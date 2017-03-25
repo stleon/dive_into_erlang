b50844654bd0fe500fe80e05ca83bf0224df67e5
ST LEON <leonst998@gmail.com>
2017-03-25 18:38:54 +0300

    some changes

--- kent/indexing.erl
+++ kent/indexing.erl
@@ -1,3 +1,23 @@
 -module(indexing).
 -compile(export_all).
 
+% { "foo" , [{3,5},{7,7},{11,13}] }
+
+clean_line(Data) when length(Data) < 3 -> [];
+
+clean_line(Data) when length(Data) >= 3 ->
+    Line = string:to_lower(string:strip(Data, both)),
+    Tokens = string:tokens(Line, " ").
+
+read(Device) ->
+    case file:read_line(Device) of
+        % line numbers
+        {ok, Data} -> [clean_line(Data) | read(Device)];
+        eof -> []
+    end.
+
+run(FileName) ->
+    {ok, Device} = file:open(FileName, read),
+    Data = read(Device),
+    ok = file:close(Device),
+    io:format(Data).
