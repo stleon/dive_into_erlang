64473120da913b8802e918a9306d4e6c76e75dc9
ST LEON <leonst998@gmail.com>
2017-04-01 13:41:44 +0300

    some improvements

--- kent/indexing.erl
+++ kent/indexing.erl
@@ -3,30 +3,23 @@
 
 % { "foo" , [{3,5},{7,7},{11,13}] }
 
-clean_line(Data) when length(Data) < 3 -> [];
+search_words(MP, Data) -> 
+    % to lower case all results
+    case re:run(Data, MP, [global, {capture, all, list}]) of
+        {match, Captured} -> lists:merge(Captured);
+        nomatch -> [] % not empty results
+    end.
 
-clean_line(Data) when length(Data) >= 3 ->
-    io:format("Get: ~s ~n", [Data]),
-    Line = string:to_lower(Data),
-    Tokens = string:tokens(Line, " "),
-    Tokens.
-
-cleaner(Data) ->
-    io:format("Get: ~s ~n", [Data]),
-    Data.
 
 read(Device) ->
+    {ok, MP} = re:compile("\\w{3,}", [caseless]),
+    
     case file:read_line(Device) of
-        % line numbers
-        % remove all, that not character
-        {ok, Data} -> [cleaner(Data) | read(Device)];
+        {ok, Data} -> [search_words(MP, Data) | read(Device)];
         eof -> []
     end.
 
 run(FileName) ->
-    {ok, MP} = re:compile("\\w{3,}", [caseless]),
-    {match, Captured} =  re:run("Oh Lol match here no", MP,
-                                                [global, {capture, all, list}]),
     {ok, Device} = file:open(FileName, [read, raw, read_ahead]),
     try read(Device)
       after file:close(Device)
